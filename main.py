38d2e124e53e1112bc33007daba801548716a0c0
gsingh <gds31.gagandeepsingh@gmail.com>
2023-02-07 21:28:46 +0000

    feat: add cli to server

--- main.py
+++ main.py
@@ -36,6 +36,11 @@ async def serve(port: int, model_artefact_path: str) -> None:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        prog=''
+        prog='treeserve',
+        description='set port number and artefact path',
+        add_help=True
     )
-    asyncio.run(serve(8000, 'artefacts/'))
+    parser.add_argument('-port', default=8000, help='set the gRPC port', required=False)
+    parser.add_argument('-path', default='/artefacts', help='set the artefacts path', required=False)
+    args = parser.parse_args()
+    asyncio.run(serve(int(args.port), args.path))
