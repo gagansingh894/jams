90166c946d22cd261a10ad61fd6ff51b5f7bb014
gsingh <gds31.gagandeepsingh@gmail.com>
2023-02-08 10:53:10 +0000

    feat: update proto

--- treeserve/api/treeserve_pb2.pyi
+++ treeserve/api/treeserve_pb2.pyi
@@ -1,7 +1,8 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -18,21 +19,20 @@ class DeployResponse(_message.Message):
     def __init__(self, model_name: _Optional[str] = ...) -> None: ...
 
 class InfoRequest(_message.Message):
-    __slots__ = []
-    def __init__(self) -> None: ...
+    __slots__ = ["model_name"]
+    MODEL_NAME_FIELD_NUMBER: _ClassVar[int]
+    model_name: str
+    def __init__(self, model_name: _Optional[str] = ...) -> None: ...
 
 class InfoResponse(_message.Message):
-    __slots__ = ["info"]
-    class InfoEntry(_message.Message):
-        __slots__ = ["key", "value"]
-        KEY_FIELD_NUMBER: _ClassVar[int]
-        VALUE_FIELD_NUMBER: _ClassVar[int]
-        key: str
-        value: str
-        def __init__(self, key: _Optional[str] = ..., value: _Optional[str] = ...) -> None: ...
-    INFO_FIELD_NUMBER: _ClassVar[int]
-    info: _containers.ScalarMap[str, str]
-    def __init__(self, info: _Optional[_Mapping[str, str]] = ...) -> None: ...
+    __slots__ = ["created_ts", "model_name", "model_version"]
+    CREATED_TS_FIELD_NUMBER: _ClassVar[int]
+    MODEL_NAME_FIELD_NUMBER: _ClassVar[int]
+    MODEL_VERSION_FIELD_NUMBER: _ClassVar[int]
+    created_ts: _timestamp_pb2.Timestamp
+    model_name: str
+    model_version: str
+    def __init__(self, model_name: _Optional[str] = ..., model_version: _Optional[str] = ..., created_ts: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
 
 class PredictRequest(_message.Message):
     __slots__ = ["input_data", "model_name"]
